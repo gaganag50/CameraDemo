68f2c71dafe3dca221ab74d8a0fca8017621eb78
Gagan Aggarwal <gaganag50@gmail.com>
2020-09-29 21:10:25 +0530

    gallery working

--- app/src/main/java/com/examples/camerademo/MainActivity.kt
+++ app/src/main/java/com/examples/camerademo/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.examples.camerademo
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.FileNotFoundException
 
 
 class MainActivity : AppCompatActivity(), IPickResult {
@@ -36,10 +40,45 @@ class MainActivity : AppCompatActivity(), IPickResult {
     }
 
     override fun onPickResult(r: PickResult?) {
-        Log.d("mainActivity", "onPickResult: ${r?.path}")
         if (r?.error != null) {
             Toast.makeText(this, r.error?.message, Toast.LENGTH_LONG).show()
         }
+        val b = setScaledBitmap(r!!.uri!!)
+        result_image.setImageBitmap(b)
+
+    }
+
+    @Throws(FileNotFoundException::class)
+    private fun getOptions(uri: Uri): BitmapFactory.Options? {
+        var options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeStream(this.contentResolver.openInputStream(uri), null, options)
+        var w = options.outWidth
+        var h = options.outHeight
+        var scale = 1
+        while (true) {
+            if (w / 2 < cardView.width || h / 2 < cardView.height) break
+            w /= 2
+            h /= 2
+            scale *= 2
+        }
+        options = BitmapFactory.Options()
+        options.inSampleSize = scale
+        return options
+    }
+
+    @Throws(FileNotFoundException::class)
+    private fun scaleDown(uri: Uri): Bitmap? {
+        return BitmapFactory.decodeStream(
+            this.contentResolver.openInputStream(uri),
+            null,
+            getOptions(uri)
+        )
+    }
+
+    private fun setScaledBitmap(uri: Uri): Bitmap {
+        return scaleDown(uri)!!
+
     }
 
 
